23f87fbf226d950b1141d156e2a4fb3b66dd8e86
Mohit Agarwalla <142979857+MoH1t1@users.noreply.github.com>
2025-12-17 04:05:37 -0800

    Organize set operations into numbered questions
    Refactored code to organize questions and improve readability.

--- Set_Assignment.py
+++ Set_Assignment.py
@@ -1,3 +1,4 @@
+# 1st Question
 A = {1,2,3,4}
 B = {1,2,7,8}
 print("U:", A | B)
@@ -5,6 +6,7 @@ print("I:", A & B)
 print("Dif:", A - B)
 print("SD:", A ^ B)
 
+# 2nd Question
 A1 = {1,2,3,4}
 B1 = {3,4,5,9}
 A1 = A1 - B1
@@ -12,6 +14,7 @@ B1 = B1 - A1
 print("Set A:", A1)
 # print("Set B:", B1)
 
+# 3rd Question
 A = {1,2}
 B = {1,2,3, 4}
 if A.issubset(B):
@@ -19,18 +22,20 @@ if A.issubset(B):
 else:
     print("A is not subset of B")
 
+# 4th Question
 s = {1,2,3,4,5}
 n = int(input("Ente limit: "))
 for i in s:
     if i > n:
         print(i)
 
-
+# 5th Question
 l3 = [1,2,3,4,5,6,54,3]
 u_s =  set(l3)
 print(u_s)
 u_l = list(u_s)
 print(u_l)
+
 
 
 
